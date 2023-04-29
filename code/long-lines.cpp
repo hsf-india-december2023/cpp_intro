b51633587a263c8dbad77b19a04c145139bc09ed
David Lange <david.lange@cern.ch>
2023-04-29 12:39:29 +0530

    add clang-format example

--- code/long-lines.cpp
+++ code/long-lines.cpp
@@ -1,14 +1,16 @@
 #include <array>
-#include <string>
 #include <iostream>
+#include <string>
 
 int main() {
-  std::array< std::string, 10 > my_strings = {"A first string", "a second string", "a third string", "and so on", "and so forth", "a testing string", "a nother testing string", "my eighth string so far", "nine, one to go", "at last the last string"};
+  std::array<std::string, 10> my_strings = {
+      "A first string",  "a second string",        "a third string",          "and so on",
+      "and so forth",    "a testing string",       "a nother testing string", "my eighth string so far",
+      "nine, one to go", "at last the last string"};
 
-  for ( const auto &str: my_strings ) {
+  for (const auto &str : my_strings) {
     std::cout << str << std::endl;
   }
-  
-    
+
   return 0;
 }
