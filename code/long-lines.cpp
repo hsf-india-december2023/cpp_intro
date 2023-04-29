c251c28b601cd193012ba8e878b574fa4598f689
David Lange <david.lange@cern.ch>
2023-04-29 12:20:37 +0530

    long lines function

---
+++ code/long-lines.cpp
@@ -0,0 +1,14 @@
+#include <array>
+#include <string>
+#include <iostream>
+
+int main() {
+  std::array< std::string, 10 > my_strings = {"A first string", "a second string", "a third string", "and so on", "and so forth", "a testing string", "a nother testing string", "my eighth string so far", "nine, one to go", "at last the last string"};
+
+  for ( const auto &str: my_strings ) {
+    std::cout << str << std::endl;
+  }
+  
+    
+  return 0;
+}
